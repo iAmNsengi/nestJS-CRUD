81c7962dc657c68246fe703d625206e7de2c4e6b
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-14 18:07:22 +0200

    chore: using pipes to alter request data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ParseIntPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto';
+// import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  singUp(@Body() dto: AuthDto) {
-    console.log(dto);
+  singUp(
+    @Body('username') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log(email, password);
 
     return this.authService.signup();
   }
