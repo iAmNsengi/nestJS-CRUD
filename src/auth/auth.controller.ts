2a6124bc8acf2cc6e38cada7604ceae337b2b05f
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-14 18:00:12 +0200

    chore: barrel export pattern auth dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  singUp(@Body() dto: any) {
+  singUp(@Body() dto: AuthDto) {
     console.log(dto);
 
     return this.authService.signup();
