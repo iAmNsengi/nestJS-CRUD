f2157aeab69c05c9a8a078720f49a7b70bb69df6
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-14 17:28:46 +0200

    fix: prisma module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { User, Bookmark } from '@prisma/client';
+// import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   login() {
     return { msg: 'I am login' };
   }
