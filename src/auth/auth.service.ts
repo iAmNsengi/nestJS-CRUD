9dfea4b92aa28eb861b3ef664d70eaabe65e5dfb
Eliezer Nsengi <avaranstudios@gmail.com>
2024-10-14 18:57:33 +0200

    feat: argon encrypt password and create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 // import { User, Bookmark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +11,19 @@ export class AuthService {
   login() {
     return { msg: 'I am login' };
   }
-  signup() {
-    return { msg: 'I am signup' };
+  async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the new user in the db
+    const newUser = await this.prisma.user.create({
+      data: {
+        username: dto.username,
+        password: hash,
+      },
+    });
+
+    // return dtothe saved user
+    return newUser;
   }
 }
